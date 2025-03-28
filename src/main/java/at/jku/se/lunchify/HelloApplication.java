f9c17d3cf468b90728151d59408af47af455d747
CarinaMossbauer <carina.mossbauer@gmx.at>
2025-03-28 10:04:10 +0100

    no changes

--- src/main/java/at/jku/se/lunchify/HelloApplication.java
+++ src/main/java/at/jku/se/lunchify/HelloApplication.java
@@ -2,6 +2,7 @@ package at.jku.se.lunchify;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -9,12 +10,15 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("base-view.fxml"));
+            Parent root = loader.load();
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/at/jku/se/lunchify/HelloController.java
+++ src/main/java/at/jku/se/lunchify/HelloController.java
@@ -1,14 +1,39 @@
 package at.jku.se.lunchify;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label label;
+    @FXML
+    private BorderPane mainPane;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void initialize() {
+        loadView("login-view.fxml");
     }
+
+    private void loadView(String fxmlFile) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent view = loader.load();
+            mainPane.setCenter(view);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
