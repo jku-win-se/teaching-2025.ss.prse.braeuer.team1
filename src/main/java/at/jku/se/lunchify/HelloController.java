732d7cd49f10c958b4ad31b309544046cfc279d4
CarinaMossbauer <carina.mossbauer@gmx.at>
2025-03-28 10:07:52 +0100

    no changes

--- src/main/java/at/jku/se/lunchify/HelloController.java
+++ src/main/java/at/jku/se/lunchify/HelloController.java
@@ -1,18 +1,10 @@
 package at.jku.se.lunchify;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloController {
     @FXML
