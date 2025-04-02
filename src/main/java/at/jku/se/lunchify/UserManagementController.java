dee6ba737f21ac5797759ab84c347ffac38ed191
CarinaMossbauer <carina.mossbauer@gmx.at>
2025-04-02 22:26:52 +0200

    updating user-creation fields, updating user-management-view

--- src/main/java/at/jku/se/lunchify/UserManagementController.java
+++ src/main/java/at/jku/se/lunchify/UserManagementController.java
@@ -1,17 +1,8 @@
 package at.jku.se.lunchify;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-
 import java.io.IOException;
 
 public class UserManagementController {
-
-    @FXML
-    protected Button newUserButton;
 
     public void onNewUserButtonClick() throws IOException {
         LunchifyApplication.baseController.showCenterView("user-creation-view.fxml");
