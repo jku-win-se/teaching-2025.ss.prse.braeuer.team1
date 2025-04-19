8630d40f86a8c24376bb1374b75d6b4239a21cb6
CarinaMossbauer <carina.mossbauer@gmx.at>
2025-04-19 19:18:53 +0200

    bugfix password-change in UserDAO

--- src/main/java/at/jku/se/lunchify/models/UserDAO.java
+++ src/main/java/at/jku/se/lunchify/models/UserDAO.java
@@ -95,7 +95,7 @@ public class UserDAO {
             ps.setString(3, user.getSurname());
             ps.setString(4, user.getType());
             ps.setBoolean(5, user.isIsactive());
-            ps.setString(6, passwordService.hashPassword(user.getPassword().trim()));
+            ps.setString(6, user.getPassword());
             ps.setInt(7, user.getUserid());
             ps.executeUpdate();
             ps.close();
@@ -110,7 +110,7 @@ public class UserDAO {
 
     public boolean insertUser(User user) throws SQLException {
         try (Connection connection = DriverManager.getConnection(jdbcUrl, username, DBpassword);
-        PreparedStatement ps = connection.prepareStatement("insert into \"User\" (email, firstname, surname, type, isactive, password) values (?,?,?,?,?,?);");){
+        PreparedStatement ps = connection.prepareStatement("insert into \"User\" (email, firstname, surname, type, isactive, password) values (?,?,?,?,?,?);")){
             ps.setString(1, user.getEmail());
             ps.setString(2, user.getFirstname());
             ps.setString(3, user.getSurname());
